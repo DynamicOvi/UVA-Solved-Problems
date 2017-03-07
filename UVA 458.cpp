a3751777c5fe1a4094cf9e20675c95313878a3e5
Mohammad Hafiz-Al-Masud <dynamicovi@gmail.com>
2017-03-08 02:05:39 +0600

    Add files via upload

---
+++ UVA 458.cpp
@@ -0,0 +1,20 @@
+//UVA-458
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char str[10000],a,b,c;
+    int d,e,f;
+    while(gets(str))
+    {
+        for(int i=0;str[i]!='\0';i++)
+        {
+            d=int(str[i])-7;
+            c=d;
+            cout<<c;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
